920b55468f68c1698ea484093294a0453b56fdf6
Toddifons <pariahs9311@gmail.com>
2023-08-18 10:45:09 +0900

    Feat : add Class itemFileRepository
    아이템 파일 레파지토리작성, 메소드 추가

---
+++ src/main/java/com/shiromi/ceobe/itemFile/repository/itemFileRepository.java
@@ -0,0 +1,14 @@
+package com.shiromi.ceobe.itemFile.repository;
+
+import com.shiromi.ceobe.item.entity.ItemEntity;
+import com.shiromi.ceobe.itemFile.entity.ItemFileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface itemFileRepository extends JpaRepository<ItemFileEntity,Long> {
+
+    List<ItemEntity> findByItemEntity(ItemEntity itemEntity2);
+
+    void deleteByItemEntity(ItemEntity itemEntity2);
+}
