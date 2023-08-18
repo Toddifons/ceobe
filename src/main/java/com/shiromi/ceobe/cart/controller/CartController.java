52655411dd095f47672d31b1beb181a5a24d1d52
yimje <imjy2@naver.com>
2023-08-18 13:40:52 +0900

    CartController, CartService, MemberController
    장바구니 컨트롤러 + 서비스, 회원 컨트롤러  httpsession 들어가있음, MemberController에서 메일인증 패스워드 인증쪽 오류있음

---
+++ src/main/java/com/shiromi/ceobe/cart/controller/CartController.java
@@ -0,0 +1,87 @@
+package com.shiromi.ceobe.cart.controller;
+
+import com.shiromi.ceobe.cart.dto.CartDTO;
+import com.shiromi.ceobe.cart.service.CartService;
+import com.shiromi.ceobe.cartItem.dto.CartItemDTO;
+import com.shiromi.ceobe.item.dto.*;
+import com.shiromi.ceobe.member.dto.MemberDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.net.HttpCookie;
+import java.util.List;
+
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/cart")
+public class CartController {
+
+    private final CartService cartService;
+
+    //장바구니 저장
+//    @GetMapping("/save")
+//    public String saveForm(@ModelAttribute ItemDTO itemDTO, Model model) {
+//        String userId = itemDTO.getUserId();
+//        cartService.save(itemDTO);
+//        model.addAttribute("item", itemDTO);
+//        return "redirect:/cart/list?userId=" + userId;
+//    }
+    @GetMapping("/saved")
+    public @ResponseBody String cartSave(@ModelAttribute ItemDTO itemDTO , HttpSession session){
+        Object member = session.getAttribute("member");
+        member = (MemberDTO) member;
+        String userId = ((MemberDTO) member).getUserId();
+        itemDTO.setUserId(userId);
+        System.out.println("장바구니저장itemDTO = " + itemDTO);
+        cartService.save(itemDTO);
+        return userId;
+    }
+
+    //장바구니 리스트
+    @GetMapping("/list")
+    public String listForm(@RequestParam("userId") String userId, Model model) {
+        List<CartItemDTO> cartItemDTOList = cartService.findAll(userId);
+        model.addAttribute("cartList", cartItemDTOList);
+        return "cartPages/cartList";
+    }
+
+    //장바구니 상세페이지
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Long id, Model model){
+        CartItemDTO cartDTO = cartService.findById(id);
+        model.addAttribute("cart",cartDTO);
+        return "cartPages/cartList";
+    }
+
+    //장바구니 삭제
+    @GetMapping("/delete")
+    public @ResponseBody String delete(@RequestParam("cartId")Long id){
+        cartService.delete(id);
+        return "success";
+    }
+
+    //장바구니 수정
+//    @GetMapping("/update")
+//    public String update(@ModelAttribute CartDTO cartDTO){
+//        System.out.println("cartDTO = " + cartDTO);
+//        cartService.update(cartDTO);
+//        return "redirect:/cart/list?userId="+cartDTO.getUserId();
+//    }
+
+    //장바구니 수량변경 ajax
+    @GetMapping("/change")
+    public @ResponseBody String change(@ModelAttribute CartDTO cartDTO){
+        System.out.println("cartDTO = " + cartDTO);
+        cartService.update(cartDTO);
+        return "success";
+    }
+
+}
---
+++ src/main/java/com/shiromi/ceobe/cart/service/CartService.java
@@ -0,0 +1,126 @@
+package com.shiromi.ceobe.cart.service;
+
+import com.shiromi.ceobe.cart.dto.CartDTO;
+import com.shiromi.ceobe.cart.entity.CartEntity;
+import com.shiromi.ceobe.cart.repository.CartRepository;
+import com.shiromi.ceobe.cartItem.dto.CartItemDTO;
+import com.shiromi.ceobe.cartItem.entity.CartItemEntity;
+import com.shiromi.ceobe.cartItem.repository.CartItemRepository;
+import com.shiromi.ceobe.item.dto.ItemDTO;
+import com.shiromi.ceobe.item.entity.ItemEntity;
+import com.shiromi.ceobe.item.repository.ItemRepository;
+import com.shiromi.ceobe.member.entity.MemberEntity;
+import com.shiromi.ceobe.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CartService {
+
+    private final CartRepository cartRepository;
+
+    private final MemberRepository memberRepository;
+
+    private final CartItemRepository cartItemRepository;
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void save(ItemDTO itemDTO) {
+        MemberEntity memberEntity = memberRepository.findByUserId(itemDTO.getUserId()).get();
+        Optional<CartEntity>cartEntity = cartRepository.findByMemberEntity(memberEntity);
+        if (cartEntity.isPresent()) {
+            CartEntity cartEntity1 = cartEntity.get();
+            cartEntity1.setMemberEntity(memberEntity);
+            cartEntity1.setId(cartEntity1.getId());
+            cartRepository.save(cartEntity1);
+
+            CartItemEntity cartItemEntity = new CartItemEntity();
+            cartItemEntity.setCartName(itemDTO.getItemName());
+            cartItemEntity.setCartCount(itemDTO.getCartCount());
+            cartItemEntity.setCartEntity(cartEntity1);
+
+            ItemEntity itemEntity =itemRepository.findById(itemDTO.getId()).get();
+            cartItemEntity.setItemEntity(itemEntity);
+
+            cartItemRepository.save(cartItemEntity);
+
+        }else {
+            CartEntity cartEntity1 = new CartEntity();
+            cartEntity1.setMemberEntity(memberEntity);
+            cartRepository.save(cartEntity1);
+
+            CartItemEntity cartItemEntity = new CartItemEntity();
+            cartItemEntity.setCartName(itemDTO.getItemName());
+            cartItemEntity.setCartCount(itemDTO.getItemCount());
+            cartItemEntity.setCartCount(itemDTO.getCartCount());
+            cartItemEntity.setCartEntity(cartEntity1);
+
+            ItemEntity itemEntity =itemRepository.findById(itemDTO.getId()).get();
+            cartItemEntity.setItemEntity(itemEntity);
+
+            cartItemRepository.save(cartItemEntity);
+        }
+    }
+
+
+    @Transactional
+    public List<CartItemDTO> findAll(String userId) {
+        MemberEntity memberEntity = memberRepository.findByUserId(userId).get();
+        Optional<CartEntity>cartEntity = cartRepository.findByMemberEntity(memberEntity);
+        if (cartEntity.isPresent()) {
+            CartEntity cartEntity1 = cartEntity.get();
+            List<CartItemEntity>cartItemEntityList = cartItemRepository.findByCartEntity(cartEntity1);
+            List<CartItemDTO> cartItemDTOList = new ArrayList<>();
+            for (CartItemEntity cartItemEntity : cartItemEntityList) {
+                CartItemDTO cartItemDTO = new CartItemDTO();
+                cartItemDTO.setId(cartItemEntity.getId());
+                cartItemDTO.setCartCount(cartItemEntity.getCartCount());
+                cartItemDTO.setItemName(cartItemEntity.getItemEntity().getItemName());
+                cartItemDTO.setItemPrice(cartItemEntity.getItemEntity().getItemPrice());
+                cartItemDTO.setItemImage(cartItemEntity.getItemEntity().getItemFileEntityList().get(0).getStoredFileNameItem());
+                cartItemDTOList.add(cartItemDTO);
+            }
+            return cartItemDTOList;
+
+        }else {
+            return null;
+        }
+    }
+    @Transactional
+    public CartItemDTO findById(Long id) {
+        Optional<CartItemEntity> cartEntityOptional = cartItemRepository.findById(id);
+        if(cartEntityOptional.isPresent()){
+            CartItemEntity cartEntity = cartEntityOptional.get();
+            CartItemDTO cartItemDTO = new CartItemDTO();
+            cartItemDTO.setId(cartEntity.getId());
+            cartItemDTO.setCartCount(cartEntity.getCartCount());
+            cartItemDTO.setItemName(cartEntity.getItemEntity().getItemName());
+            cartItemDTO.setItemPrice(cartEntity.getItemEntity().getItemPrice());
+            cartItemDTO.setItemImage(cartEntity.getItemEntity().getItemFileEntityList().get(0).getStoredFileNameItem());
+            return cartItemDTO;
+        }else{
+            return null;
+        }
+    }
+    @Transactional
+    public List<CartItemDTO> delete(Long id) {
+        cartItemRepository.deleteById(id);
+        return null;
+    }
+    @Transactional
+    public void update(CartDTO cartDTO) {
+        CartItemEntity cartItemEntity = cartItemRepository.findById(cartDTO.getId()).get();
+        cartItemEntity.setCartCount(cartDTO.getCartCount());
+        cartItemRepository.save(cartItemEntity);
+    }
+}
