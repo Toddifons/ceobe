e21fb3e9709daef986664d330e8b801f3613ac50
Toddifons <pariahs9311@gmail.com>
2023-08-18 10:12:28 +0900

    Feat : add ItemSave method ItemEntity
    엔티티 DTO 변환 메소드 작성

--- src/main/java/com/shiromi/ceobe/item/entity/ItemEntity.java
+++ src/main/java/com/shiromi/ceobe/item/entity/ItemEntity.java
@@ -2,16 +2,21 @@ package com.shiromi.ceobe.item.entity;
 
 import com.shiromi.ceobe.common.entity.BaseEntity;
 import com.shiromi.ceobe.item.dto.ItemDTO;
+import com.shiromi.ceobe.itemFile.entity.ItemFileEntity;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "item_table")
+@NoArgsConstructor
 public class ItemEntity extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -40,6 +45,11 @@ public class ItemEntity extends BaseEntity {
     @Column
     private int itemSellCount = 0;
 
+    //item(상품) : item_file(상품이미지) = 1 : M
+    @OneToMany(mappedBy = "itemEntity", cascade = CascadeType.REMOVE, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<ItemFileEntity> itemFileEntityList = new ArrayList<>();
+
+
     @Builder
     public ItemEntity(Long id, String itemName, int itemPrice, String itemContents, int itemCount, int fileAttachedItem, String itemCategory, int itemSellCount) {
         this.id = id;
@@ -53,17 +63,37 @@ public class ItemEntity extends BaseEntity {
         this.itemSellCount = itemSellCount;
     }
 
-    public ItemDTO toItemDTO() {
-        return ItemDTO.builder()
-                .id(id)
-                .itemName(itemName)
-                .itemPrice(itemPrice)
-                .itemContents(itemContents)
-                .itemCount(itemCount)
-                .fileAttachedItem(fileAttachedItem)
-                .itemCategory(itemCategory)
-                .itemSellCount(itemSellCount)
-                .build();
+    public static ItemEntity toItemSaveEntity(ItemDTO itemDTO) {
+        ItemEntity itementity = new ItemEntity();
+        itementity.setItemName(itemDTO.getItemName());
+        itementity.setItemPrice(itemDTO.getItemPrice());
+        itementity.setItemContents(itemDTO.getItemContents());
+        itementity.setItemCount(itemDTO.getItemCount());
+        itementity.setItemCategory(itemDTO.getItemCategory());
+        itementity.setFileAttachedItem(0);
+        return itementity;
+    }
+
+    public static ItemEntity toItemSaveFileEntity(ItemDTO itemDTO) {
+        ItemEntity itementity = new ItemEntity();
+        itementity.setItemName(itemDTO.getItemName());
+        itementity.setItemPrice(itemDTO.getItemPrice());
+        itementity.setItemContents(itemDTO.getItemContents());
+        itementity.setItemCount(itemDTO.getItemCount());
+        itementity.setItemCategory(itemDTO.getItemCategory());
+        itementity.setFileAttachedItem(1);
+        return itementity;
+    }
+    public static ItemEntity toItemUpdateEntity(ItemDTO itemDTO) {
+        ItemEntity itementity = new ItemEntity();
+        itementity.setId(itemDTO.getId());
+        itementity.setItemName(itemDTO.getItemName());
+        itementity.setItemPrice(itemDTO.getItemPrice());
+        itementity.setItemContents(itemDTO.getItemContents());
+        itementity.setItemCount(itemDTO.getItemCount());
+        itementity.setItemCategory(itemDTO.getItemCategory());
+        itementity.setFileAttachedItem(1);
+        return itementity;
     }
 
 
