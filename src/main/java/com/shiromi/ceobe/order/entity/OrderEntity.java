2c9e65127d412a35cf4b41b317cc66409a919007
Toddifons <pariahs9311@gmail.com>
2023-08-18 10:21:32 +0900

    Feat : add value orderItemEntityList, OrderEntity Class
    오더아이템 엔티티를 원투매니로 담는 밸류 추가

--- src/main/java/com/shiromi/ceobe/order/entity/OrderEntity.java
+++ src/main/java/com/shiromi/ceobe/order/entity/OrderEntity.java
@@ -2,9 +2,12 @@ package com.shiromi.ceobe.order.entity;
 
 import com.shiromi.ceobe.common.entity.BaseEntity;
 import com.shiromi.ceobe.member.entity.MemberEntity;
+import com.shiromi.ceobe.orderItem.entity.OrderItemEntity;
 import lombok.Builder;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "order")
@@ -25,6 +28,11 @@ public class OrderEntity extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private MemberEntity memberEntity;
+
+    //    order(주문) : order_item(주문상품) = 1 : M
+    @OneToMany(mappedBy = "orderEntity", cascade = CascadeType.REMOVE, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<OrderItemEntity> orderItemEntityList = new ArrayList<>();
+
 
     @Builder
     public OrderEntity(Long orderId, String orderName, String orderStatus, String review, MemberEntity memberEntity) {
