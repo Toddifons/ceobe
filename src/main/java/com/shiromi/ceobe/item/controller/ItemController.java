4c74669445f5946e1700bd4f2f369a7cfb49190c
Toddifons <pariahs9311@gmail.com>
2023-08-18 11:06:29 +0900

    Feat : add update method, ItemController

--- src/main/java/com/shiromi/ceobe/item/controller/ItemController.java
+++ src/main/java/com/shiromi/ceobe/item/controller/ItemController.java
@@ -17,21 +17,32 @@ import java.io.IOException;
 public class ItemController {
     private final ItemService itemService;
 
-
+    //상품 저장 화면
     @GetMapping("/item/save")
     public String saveForm() {
         return "itemPages/itemSave";
     }
-
+    //상품 저장처리
     @PostMapping("/item/save")
     public String save(@ModelAttribute ItemDTO itemDTO) throws IOException {
         itemService.save(itemDTO);
         return "redirect:/item/main";
     }
+    //상품 수정 화면
     @GetMapping("/item/update/{id}")
     public String updateForm(@PathVariable Long id, Model model){
         ItemDTO itemDTO = itemService.findById(id);
         model.addAttribute("item",itemDTO);
         return "itemPages/itemUpdate";
     }
+    //상품 수정 처리
+    @PostMapping("/item/update")
+    public String update (@ModelAttribute ItemDTO itemDTO,Model model) throws IOException{
+        System.out.println("itemDTO = " + itemDTO + ", model = " + model);
+        itemService.update(itemDTO);
+        ItemDTO itemDTO1 = itemService.findById(itemDTO.getId());
+        model.addAttribute("board",itemDTO1);
+        return "redirect:/item/main";
+    }
+
 }
