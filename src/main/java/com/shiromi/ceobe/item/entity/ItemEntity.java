61251a78f4ba9677d8b1f9e17c58d82f324150f1
Toddifons <pariahs9311@gmail.com>
2023-08-18 09:33:37 +0900

    Feat : modify annotation, ItemEntity
    게터 세터 추가

--- src/main/java/com/shiromi/ceobe/item/entity/ItemEntity.java
+++ src/main/java/com/shiromi/ceobe/item/entity/ItemEntity.java
@@ -7,8 +7,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "item_table")
 public class ItemEntity extends BaseEntity {
