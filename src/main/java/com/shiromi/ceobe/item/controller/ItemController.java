93ea1198defc296a700060e3ffad46dd4ec7f3be
Toddifons <pariahs9311@gmail.com>
2023-08-18 09:43:19 +0900

    Feat : add itemSave, ItemService Class
    API 메소드 추가

--- src/main/java/com/shiromi/ceobe/item/controller/ItemController.java
+++ src/main/java/com/shiromi/ceobe/item/controller/ItemController.java
@@ -1,16 +1,25 @@
 package com.shiromi.ceobe.item.controller;
 
+import com.shiromi.ceobe.item.dto.ItemDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class ItemController {
+    private final ItemService itemService;
 
 
     @GetMapping("/item/save")
     public String saveForm() {
         return "itemPages/itemSave";
+    }
+
+    @PostMapping("/item/save")
+    public String save(@ModelAttribute ItemDTO itemDTO) {
+        itemService.save(itemDTO);
     }
 }
