727212162a80122fb0a6ed317d08323d56288722
Toddifons <pariahs9311@gmail.com>
2023-08-18 15:51:00 +0900

    Feat : add WebConfig Class
    파일 저장 위치와 검증없이 요청할수 있는 URL 설정

---
+++ src/main/java/com/shiromi/ceobe/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.shiromi.ceobe.config;
+
+
+import com.shiromi.ceobe.common.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private String resourcePath = "/upload/**";
+    private String savePath = "file:///C:/springBootImg/";
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(resourcePath).addResourceLocations(savePath);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor()) //인터셉터로 등록할 클래스
+                .order(1) // 해당 인터셉터의 우선순위
+                .addPathPatterns("/**") //인터셉터로 체크할 주소 (모든 주소)
+                .excludePathPatterns("/popup","/apis/**","/member/searchPasswordUpdate","/member/searchPassword","/createCookie","smtp.**","/member/mailConfirm","/question/list","/cart/change","/cart/saved","/item/**","/member/duplicate-check-userId","/", "/member/save", "/member/login", "/member/logout", "/member/kakao","/item/main", "/js/**", "/css/**", "/jsp/**","/style/**", "/error", "/img/**","/product/**", "/*.ico", "/favicon/**","/upload/**"); //예외할 주소, 로그인 안해도 사용 가능
+
+    }
+}
