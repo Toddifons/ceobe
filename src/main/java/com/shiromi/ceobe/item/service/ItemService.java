5d92f5af4368bb13be32cd7b4260af41f1dd5513
Toddifons <pariahs9311@gmail.com>
2023-08-18 09:53:20 +0900

    Feat : add save, ItemService
    아이템 서비스 추가

---
+++ src/main/java/com/shiromi/ceobe/item/service/ItemService.java
@@ -0,0 +1,15 @@
+package com.shiromi.ceobe.item.service;
+
+import com.shiromi.ceobe.item.dto.ItemDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ItemService {
+
+    public void save(ItemDTO itemDTO) {
+
+    }
+
+}
