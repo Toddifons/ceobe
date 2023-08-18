cb27c990abffd78da4e57de814dd13b925de4760
Toddifons <pariahs9311@gmail.com>
2023-08-18 10:50:56 +0900

    Feat : modify save method, ItemService
    세이브 메소드 작성

--- src/main/java/com/shiromi/ceobe/item/service/ItemService.java
+++ src/main/java/com/shiromi/ceobe/item/service/ItemService.java
@@ -1,15 +1,43 @@
 package com.shiromi.ceobe.item.service;
 
 import com.shiromi.ceobe.item.dto.ItemDTO;
+import com.shiromi.ceobe.item.entity.ItemEntity;
+import com.shiromi.ceobe.item.repository.ItemRepository;
+import com.shiromi.ceobe.itemFile.entity.ItemFileEntity;
+import com.shiromi.ceobe.itemFile.repository.ItemFileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 public class ItemService {
+    private final ItemRepository itemRepository;
+    private final ItemFileRepository itemFileRepository;
 
-    public void save(ItemDTO itemDTO) {
-
+    public Long save(ItemDTO itemDTO) throws IOException {
+        if (itemDTO.getItemFile().get(0).isEmpty()) {
+            System.out.println("파일 없음");
+            ItemEntity itemEntity = ItemEntity.toItemSaveEntity(itemDTO);
+            return itemRepository.save(itemEntity).getId();
+        } else {
+            System.out.println("파일 있음");
+            ItemEntity itemEntity = ItemEntity.toItemSaveFileEntity(itemDTO);
+            Long savedId = itemRepository.save(itemEntity).getId();
+            ItemEntity entity = itemRepository.findById(savedId).get();
+            for (MultipartFile itemFile : itemDTO.getItemFile()) {
+                String originalFileNameItem = itemFile.getOriginalFilename();
+                String storedFileNameItem = System.currentTimeMillis() + "-" + originalFileNameItem;
+                String savePath = "C:\\springboot_img_final\\" + storedFileNameItem;
+                itemFile.transferTo(new File(savePath));
+                ItemFileEntity itemFileEntity = ItemFileEntity.toSaveItemFileEntity(entity, originalFileNameItem, storedFileNameItem);
+                itemFileRepository.save(itemFileEntity);
+            }
+            return savedId;
+        }
     }
 
 }
