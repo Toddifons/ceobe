1578a00b31407026014340a5f98c3ee1af621b1e
yimje <imjy2@naver.com>
2023-08-18 19:26:12 +0900

    feat : add SecurityConfig

---
+++ src/main/java/com/shiromi/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,35 @@
+package com.shiromi.config.auth;
+
+import com.shiromi.ceobe.member.entity.MemberEntity;
+import com.shiromi.ceobe.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    @Autowired
+    private final MemberRepository memberRepository;
+
+    //login 동작을 할 때 실행됨.
+    //view에서 파라미터로 넘어온 username을 토대로 User 찾아서 객체를 PrincipalDetails 객체에 저장하는 메소드
+    @Override
+    public MemberDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        MemberEntity userEntity = memberRepository.findByMemberEmail(email);
+
+        if(userEntity != null) {
+            return new PrincipalDetails(userEntity);
+        }
+        return null;
+        //return new UsernameNotFoundException("해당 유저를 찾을 수 없습니다.");
+        //-> PrincipalDetails 객체가 DB에서 User정보를 가져와 필드로 저장한다.
+        //-> PrincipalDetails가 리턴되면 Authentication 내부에 저장된다.
+        // 그런 후 Authentication이 스프링 시큐리티의 session 내부에 들어가게 된다.
+
+    }
+}
