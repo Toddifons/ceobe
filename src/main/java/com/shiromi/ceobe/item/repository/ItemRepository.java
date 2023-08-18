f9a288fcdf1e0bdd4184ea2839ac80bf55ffa888
Toddifons <pariahs9311@gmail.com>
2023-08-18 16:30:20 +0900

    refactor : fix unused value
    사용되지 않는 밸류와 임포트 제거

--- src/main/java/com/shiromi/ceobe/item/repository/ItemRepository.java
+++ src/main/java/com/shiromi/ceobe/item/repository/ItemRepository.java
@@ -7,12 +7,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import javax.persistence.EntityManager;
 import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<ItemEntity, Long> {
-    EntityManager entityManager = null;
 
     Optional<ItemEntity> findByItemName(String orderName);
 
