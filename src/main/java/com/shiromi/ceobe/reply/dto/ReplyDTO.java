0f4b8c0a5f79d09221a69072d95e97747f160db9
Toddifons <pariahs9311@gmail.com>
2023-08-18 20:57:45 +0900

    rename : rename ReplyDTO
    리플 DTO 옮김

---
+++ src/main/java/com/shiromi/ceobe/reply/dto/ReplyDTO.java
@@ -0,0 +1,31 @@
+package com.shiromi.ceobe.reply.dto;
+
+import com.shiromi.ceobe.reply.entity.ReplyEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class ReplyDTO {
+    private Long id;
+    private String replyContents;
+    private String replyName;
+    private LocalDateTime replyDate;
+    private Long questionId;
+    private Long memberId;
+
+
+    public static ReplyDTO toReplyDTO(ReplyEntity replyEntity) {
+        ReplyDTO replyDTO = new ReplyDTO();
+        replyDTO.setId(replyEntity.getId());
+        replyDTO.setReplyContents(replyEntity.getReplyContents());
+        replyDTO.setReplyName(replyEntity.getReplyName());
+        replyDTO.setReplyDate(replyEntity.getCreatedTime());
+        return replyDTO;
+    }
+}
+
