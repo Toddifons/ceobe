807d62436a72a8abaf0ccd88b37dd76e9e95d524
Toddifons <pariahs9311@gmail.com>
2023-08-18 11:39:22 +0900

    comment : modify saveForm, OrderController
    로그 작성

--- src/main/java/com/shiromi/ceobe/order/controller/OrderController.java
+++ src/main/java/com/shiromi/ceobe/order/controller/OrderController.java
@@ -2,11 +2,13 @@ package com.shiromi.ceobe.order.controller;
 
 import com.shiromi.ceobe.item.dto.ItemDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class OrderController {
@@ -14,7 +16,7 @@ public class OrderController {
     //주문상세페이지 이동
     @GetMapping("/order/save")
     public String saveForm(@ModelAttribute ItemDTO itemDTO, Model model) {
-        System.out.println("itemDTO = " + itemDTO + ", model = " + model);
+        log.info("itemDTO = {}, model = {}",itemDTO, model);
         model.addAttribute("item", itemDTO);
         return "orderPages/orderSave";
     }
